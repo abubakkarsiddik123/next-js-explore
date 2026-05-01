7b17c34451b977d7a4f2ab792d1f2f446e40ce23
abubakkarsiddik123 <abu089604@gmail.com>
2026-05-02 01:14:36 +0600

    explore metadata and og image with next js

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+export const metadata = {
+  title: 'contact',
+  description: '...',
+}
+
 const ContactPage = () => {
     return (
         <div className='text-3xl text-center text-red-500'>
